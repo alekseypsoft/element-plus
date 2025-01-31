b10455cc111d3d4b0aaa1b1dca5381c09ea588f7
aleksey <av.pavlow@gmail.com>
2025-01-31 16:07:11 +0300

    CR-иконка в панели

--- src/components/layouts/toolbar/Toolbar.vue
+++ src/components/layouts/toolbar/Toolbar.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-<ToolbarSite></ToolbarSite>
-  <ToolbarData></ToolbarData>
-  <ToolbarLogic></ToolbarLogic>
+  <el-row>
+    <el-col>
+      <img src="../../../assets/toolbar/site/cr.svg" alt=""/>
+    </el-col>
+    <el-col>
+      <ToolbarSite></ToolbarSite>
+    </el-col>
+    <el-col>
+      <ToolbarData></ToolbarData>
+    </el-col>
+    <el-col>
+      <ToolbarLogic></ToolbarLogic>
+    </el-col>
+  </el-row>
+
 </template>
 
 <style scoped>
